295bf8bdfa51df233963feb55787e7595de16a84
Paulo Teodoro <paulolimrs@outlook.com>
2022-05-05 22:54:32 -0300

    Logout API

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -30,4 +30,11 @@ class AuthController extends Controller
             "token" => $token
         ]);
     }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json(['success' => true]);
+    }
 }
