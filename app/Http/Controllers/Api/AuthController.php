17a139842a339512ec2f6a2253c5c0e5f45625f0
Paulo Teodoro <paulolimrs@outlook.com>
2022-05-05 22:47:56 -0300

    Auth API with Sanctun

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function auth(AuthRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        // Revoke all tokens
+        $user->tokens()->delete();
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+     
+        return response()->json([
+            "token" => $token
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\{
+    AuthController,
     CourseController,
     LessonController,
     ModuleController,
@@ -10,19 +11,23 @@ use App\Http\Controllers\Api\{
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
-
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
-
-Route::get('/supports', [SupportController::class, 'index']);
-Route::get('/user-supports', [SupportController::class, 'userSupports']);
-Route::post('/supports', [SupportController::class, 'store']);
-
-Route::post('/replies', [ReplySupportController::class, 'createReply']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::get('/user-supports', [SupportController::class, 'userSupports']);
+    Route::post('/supports', [SupportController::class, 'store']);
+    
+    Route::post('/replies', [ReplySupportController::class, 'createReply']);
+});
 
 Route::get('/', function() {
     return response()->json([
