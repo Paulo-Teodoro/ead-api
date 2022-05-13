10ad31b2a5f7e0a81a9081838d74653ed344fc7b
Paulo Teodoro <paulolimrs@outlook.com>
2022-05-12 21:09:21 -0300

    mark lesson viewed api

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     
     Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
     Route::get('/lessons/{id}', [LessonController::class, 'show']);
+
+    Route::post('/lessons/viewed', [LessonController::class, 'viewed']);
     
     Route::get('/supports', [SupportController::class, 'index']);
     Route::get('/user-supports', [SupportController::class, 'userSupports']);
